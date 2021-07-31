1405b83e93a9db47f83a719f4fccf0e6b5c999f3
Naman Puri <namanpuri80@gmail.com>
2021-07-31 16:21:37 -0400

    Challenge

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
+#use Flask to render a template, redirecting to another url, and creating a URL.
 from flask import Flask, render_template, redirect, url_for
+#use PyMongo to interact wit Mongo database.
 from flask_pymongo import PyMongo
-from splinter import Browser
+#use the scraping code, convert from Jupyter notebook to Python.
+import scraping
+
+#set up Flask
 app = Flask(__name__)
+
+#Tell Python how to connect to Mongo using PyMongo
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
+# define the route for the HTML page
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
+#set up our scraping route
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
